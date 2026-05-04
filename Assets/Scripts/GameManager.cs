c31a0ed934e8e1bab5e1a0cb5a217059c61313dc
Zaratreus <pedrolevert2@gmail.com>
2026-05-04 17:21:38 -0300

    Add GameManager pt2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,43 @@
+using DG.Tweening;
+using Ebac.Core.Singleton;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Ebac.Core.Singleton;
 
 public class GameManager : Singleton<GameManager>
 {
+    [Header("Player")]
+    public GameObject playerPrefab;
+
+    [Header("Enemies")]
+    public List<GameObject> enemies;
+
+    [Header("References")]
+    public Transform startPoint;
+
+
+    [Header("Animation")]
+    public float duration = .2f;
+    public float delay = .05f;
+    public Ease ease = Ease.OutBack;
+
+    private GameObject _currentPlayer;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        _currentPlayer = Instantiate(playerPrefab);
+        _currentPlayer.transform.position = startPoint.transform.position;
+        _currentPlayer.transform.DOScale(0, duration).SetEase(ease).From();
+    }
 
 }
