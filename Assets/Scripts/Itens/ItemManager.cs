4a4efd600c93c760f2c33ce1ccef0e7449a4032a
Zaratreus <pedrolevert2@gmail.com>
2026-05-15 13:16:06 -0300

    add interface

--- Assets/Scripts/Itens/ItemManager.cs
+++ Assets/Scripts/Itens/ItemManager.cs
@@ -1,27 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using Ebac.Core.Singleton;
 
-public class ItemManager : Singleton<ItemManager>
+public class ItemManager : MonoBehaviour
 {
-   
+   public static ItemManager Instance;
+
     public int coins;
+    public TextMeshProUGUI uiTextCoins;
+
+
+    private void Awake()
+    {
+
+        if (Instance == null)
+            Instance = this;
+        else Destroy(gameObject);
+
+    }
+
 
     private void Start()
     {
         Reset();
     }
+       
+
+    
 
     private void Reset()
     {
         coins = 0;
+        UpdateUI();
 
     }
 
     public void AddCoins(int amount = 1)
     {
         coins += amount;
+    }
+
+    private void UpdateUI()
+    {
+        //uiTextCoins.text = coins.ToString();
+        UIInGameManager.UpdateTextCoins(coins.ToString());
     }
 
 }
