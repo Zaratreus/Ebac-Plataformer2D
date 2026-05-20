3e86000c8ce279494c0677d2ad491a20a6d40bca
Zaratreus <pedrolevert2@gmail.com>
2026-05-19 23:14:16 -0300

    add sfx jump and run

---
+++ Assets/Scripts/AudioPlayHelper.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayHelper : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public void Play()
+    {
+        audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/AudioRandomPlayAudioCLips.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioRandomPlayAudioCLips : MonoBehaviour
+{
+    public List<AudioClip> audioClipList;
+    public List<AudioSource> audioSourceList;
+    private int _index = 0;
+    public void PlayRandom()
+    {
+        if (_index >= audioSourceList.Count) _index = 0;
+        var audioSource = audioSourceList[_index];
+        _index++;
+    }
+}
